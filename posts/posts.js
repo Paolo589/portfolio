c5aa68bc017e49abaddc33cbd9463687307cde68
Paolo589 <103574532+Paolo589@users.noreply.github.com>
2026-09-22 12:34:18 +0200

    corr reel

--- posts/posts.js
+++ posts/posts.js
@@ -15,7 +15,7 @@ export const posts = [
     id: 53,
     title: "Reel MCS Binder",
     slug: "reel_mcs_binder",
-    anteprima_img: "53_Reel_MCS_Binder/Reel_MCS_Binder.png",
+    anteprima_img: "53-Reel_MCS_Binder/Reel_MCS_Binder.png",
     anteprima_video: "53-Reel_MCS_Binder/Reel_MCS_Binder.mp4",
     galleria: [
       "53-Reel_MCS_Binder/Reel_MCS_Binder.mp4",
